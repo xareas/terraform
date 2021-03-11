bfb876ace842c5eb1e72bfcf2224b0ca536c71d4
Stywar <franco111988@gmail.com>
2021-03-11 02:31:51 -0500

    se agrego code

--- azure-terraform/kuebernetes/main.tf
+++ azure-terraform/kuebernetes/main.tf
@@ -23,7 +23,8 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   default_node_pool {
     name            = "agentpool"
     node_count      = var.node_count
-    vm_size         = "Standard_DS1_v2"
+    #vm_size         = "Standard_DS1_v2"
+	vm_size         = "Standard_B2s"
   }
 
   service_principal {

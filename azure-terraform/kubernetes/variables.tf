d6fa470dd13f64b9a34daeb36ab2c64f3e94503b
Stywar <franco111988@gmail.com>
2021-10-23 17:42:24 -0500

    change main

--- azure-terraform/kubernetes/variables.tf
+++ azure-terraform/kubernetes/variables.tf
@@ -17,13 +17,13 @@ variable node_count {
 
 
 variable dns_prefix {
-  default = "k8sAforo255sv"
+  default = "k8sAforo255fv"
 }
 
 variable cluster_name {
-  default = "k8sAforo255sv"
+  default = "k8sAforo255fv"
 }
 
 variable resource_group {
-  default = "kubernetesAforo255"
+  default = "kubernetesAforo255fv"
 }
